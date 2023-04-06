9254962d78440f72f8fbb6c180d6785b892da89a
imranar3 <root@DESKTOP-IVTL7CD.localdomain>
2023-04-06 19:13:41 -0400

    Initial commit

--- frontend/src/pages/myRecipes/index.js
+++ frontend/src/pages/myRecipes/index.js
@@ -4,6 +4,11 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 function MyRecipes() {
     return (
       <>
+        <div class="container container-1000 container-min-1000 mt-8">
+          <div class="d-flex justify-content-between mb-2">
+              <h1>My Recipes</h1>
+          </div>
+        </div>
       </>   
     );
   }
