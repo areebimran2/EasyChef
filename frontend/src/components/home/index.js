75b8229f9ed25c09e8b7d7c43a616ec7f3339785
xujet <jett.xu@mail.utoronto.ca>
2023-04-06 17:41:21 -0400

    Almost done homepage

--- frontend/src/components/home/index.js
+++ frontend/src/components/home/index.js
@@ -7,8 +7,8 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 //    }
 //}
 
-const HomeComp = () => {
-    var [recipes, setRecipes] = useState([])
+const HomeComp = (props) => {
+    var [recipes, setRecipes] = useState()
 
     useEffect(() => {
         fetch('http://localhost:8000/recipes/all/',
@@ -21,12 +21,34 @@ const HomeComp = () => {
 
     console.log(recipes)
 
+    if (recipes === undefined){
+        return (
+            <>
+            <div className="container-1000 mt-5 ms-auto me-auto">
+                <h1><a href="extendRecipeHistory.html" className="header-link mb-2">Popular Recipes</a></h1>
+                <div className="d-flex flex-row flex-wrap gap-3">
+                    <h2>No recipes created yet!</h2>
+                </div>
+            </div>
+        </>
+        )
+    }
+    
+
     return(
         <>
             <div className="container-1000 mt-5 ms-auto me-auto">
                 <h1><a href="extendRecipeHistory.html" className="header-link mb-2">Popular Recipes</a></h1>
                 <div className="d-flex flex-row flex-wrap gap-3">
-                    
+                    {recipes.results.map((recipe, i) => (
+                        <div className="card card-custom infocard bg-white text-black" key={i}>
+                            <a href="recipe2.html">
+                            <img className="card-img" src={recipe.picture} alt={recipe.name}/>
+                            <div className="card-body">
+                              <div className="card-title">{recipe.name}</div>
+                            </div></a>
+                        </div>
+                    ))}
                 </div>
             </div>
         </>
