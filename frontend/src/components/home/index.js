b0967b23fe16b2482277ed7c4829ad9e519202cf
imranar3 <root@DESKTOP-IVTL7CD.localdomain>
2023-04-11 01:25:39 -0400

    Tweaked recipe info cards

--- frontend/src/components/home/index.js
+++ frontend/src/components/home/index.js
@@ -1,7 +1,8 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../custom.css';
 import '../../../node_modules/bootstrap/dist/js/bootstrap.min.js';
-import { Link } from "react-router-dom";
+import notfound from '../MyRecipes/Card/local-file-not-found.png';
+import { useNavigate } from 'react-router-dom';
 
 //class Home extends React.Component{
 //    render(){
@@ -9,83 +10,75 @@ import { Link } from "react-router-dom";
 //}
 
 const HomeComp = () => {
+    let navigate = useNavigate()
+
     var [recipes, setRecipes] = useState();
-    const dietCode = {"NONE": "N/A", "VEGAN": "Vegan", "VEG": "Vegetarian", 
-    "GLUTENF": "Gluten-free", "LCARB": "Low Carb", "KT": "Keto", "LF": "Low-fat"}
-    const cuisineCode = {"NONE": "N/A", "CN": "Chinese", "CR": "Creole", 
-    "FR": "French", "IN": "Indian", "JP": "Japanese", "KO": "Korean", "ME": "Middle-Eastern", "WE": "Western"}
 
     useEffect(() => {
         fetch('http://localhost:8000/recipes/all/',
-        {
-            method: 'GET'
-        })
+            {
+                method: 'GET'
+            })
             .then(response => response.json())
             .then(data => setRecipes(data))
     }, [])
 
     //console.log(recipes)
 
-    if (recipes === undefined){
+    if (recipes === undefined) {
         return (
             <>
-            <div className="container-1000 mt-5 ms-auto me-auto">
-                <h1><a href="extendRecipeHistory.html" className="header-link mb-2">Popular Recipes</a></h1>
-                <div className="d-flex flex-row flex-wrap gap-3">
-                    <h2>No recipes created yet!</h2>
+                <div className="container-1000 mt-5 ms-auto me-auto">
+                    <h1><a href="extendRecipeHistory.html" className="header-link mb-2">Popular Recipes</a></h1>
+                    <div className="d-flex flex-row flex-wrap gap-3">
+                        <h2>No recipes created yet!</h2>
+                    </div>
                 </div>
-            </div>
-        </>
+            </>
         )
     }
-    
 
-    return(
+
+    return (
         <>
             <div className="container-1000 mt-5 ms-auto me-auto">
                 <h1><a href="extendRecipeHistory.html" className="header-link mb-2">Popular Recipes</a></h1>
                 <div className="d-flex flex-row flex-wrap gap-3">
-                    {recipes.results.sort((a, b) => a.num_fav < b.num_fav ? 1: -1).slice(0, 6).map((recipe, i) => (
-                        /*
-                        <div className="card card-custom infocard bg-white text-black" key={i}>
-                            <Link to={{
-                                pathname: `/recipe/${recipe.id}`
-                            }}>
-                            <img className="card-img" src={recipe.picture} alt={recipe.name}/>
-                            <div className="card-body">
-                              <div className="card-title">{recipe.name}</div>
-                              <div className="card-favs">Number of favourites: {recipe.num_fav}</div>
-                            </div></Link>
-                        </div>
-                        */
-                        <div className="card infocard bg-white text-black" key={i}>
-                        <img className="card-img" src={recipe.picture} alt={recipe.name}/>
-                        <div className="card-body hidedetails">
-                            <div className="card-title">{recipe.name}</div>
-                            <div className="card-creator">Number of Favourites: {recipe.num_fav}</div>
-                            <div className="d-flex justify-content-between">
-                                <div>
-                                    <Link to= {{
-                                        pathname: `/recipe/${recipe.id}`
-                                    }}>
-                                    <button className="btn-sm btn-outline-brown px-3">View</button></Link>
+                    {recipes.results.sort((a, b) => a.num_fav < b.num_fav ? 1 : -1).slice(0, 6).map((recipe, i) => (
+                        <div class="card homecard bg-white text-black" key={recipe.id}>
+                            <img class="card-img" src={recipe.picture !== null ? recipe.picture : notfound} alt=""/>
+                            <div class="card-body hidedetails">
+                                <div class="card-title mt-3">{recipe.name}</div>
+                                <div class="d-flex">
+                                    {[...Array(6).keys()]
+                                        .filter((item) => (item > 0))
+                                        .map((index, item) => (
+                                            item < recipe.ave_rating ? <i key={index} class="mb-auto fa-solid fa-star"></i> : <i key={index} class="mb-auto fa-regular fa-star"></i>
+                                        ))}
+                                    <p class="mx-1 mb-auto">{recipe.ave_rating}</p>
+                                </div>
+                                <div class="mt-2 mb-2">Number of Favourites: {recipe.num_fav}</div>
+                                <div class="d-flex justify-content-around">
+                                    <div>
+                                        <button type="button" class="btn-sm btn-outline-brown px-3 mb-1" onClick={() => {navigate(`/recipe/${recipe.id}`)}}>View</button>
+                                    </div>
+                                </div>
+                                <div class="card recipecard mt-2 p-3 bg-light-brown">
+                                    <ul class="list-unstyled mb-0 lh-lg">
+                                        <li><span class="fw-bold">Diet:</span> Vegan</li>
+                                        <li><span class="fw-bold">Cuisine:</span> Western</li>
+                                        <li><span class="fw-bold">Cooking time:</span> {recipe.cooking_time} minutes</li>
+                                        <li><span class="fw-bold">Prep time:</span> {recipe.prep_time} minutes</li>
+                                        <li><span class="fw-bold">Servings: </span>{recipe.serving_size}</li>
+                                    </ul>
                                 </div>
                             </div>
-                            <div className="card recipecard mt-2 p-3 bg-light-brown">
-                                <ul className="list-unstyled mb-0 lh-lg">
-                                    <li><span className="fw-bold">Diet:</span> {dietCode[recipe.diet]}</li>
-                                    <li><span className="fw-bold">Cuisine:</span> {cuisineCode[recipe.cuisine]}</li>
-                                    <li><span className="fw-bold">Cooking time:</span> {recipe.cooking_time} minutes</li>
-                                    <li><span className="fw-bold">Servings: </span> {recipe.serving_size}</li>
-                                </ul>
-                            </div>
                         </div>
-                    </div>
                     ))}
                 </div>
             </div>
         </>
-        
+
     )
 }
 
