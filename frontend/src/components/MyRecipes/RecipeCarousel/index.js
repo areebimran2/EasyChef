fcf6f073a8f418223af8108e0b5a139d187e1b6c
imranar3 <root@DESKTOP-IVTL7CD.localdomain>
2023-04-11 03:25:53 -0400

    Refactored search page and added pagination

--- frontend/src/components/MyRecipes/RecipeCarousel/index.js
+++ frontend/src/components/MyRecipes/RecipeCarousel/index.js
@@ -1,78 +1,7 @@
+import Pagination from "../../pagination";
 import Card from "../Card";
 
 const RecipeCarousel = ({ recipes, hasEnded, setPage, page, count, id, deleted, setDeleted }) => {
-    const Pagination = () => {
-        if (Math.ceil(page / 5) === 1 && (page < 5 || Math.ceil(count / 3) <= 5)) {
-            return (
-                <>
-                    {[...Array(5).keys()]
-                        .filter((item) => (item > 1))
-                        .filter((item) => (item <= Math.min(4, Math.ceil(count / 3) - 1)))
-                        .map((item) => (
-                            <button key={item} disabled={item === page} onClick={() => setPage(item)} class="btn rounded-pill btn-outline-brown" type="button">
-                                <span>{item}</span>
-                            </button>
-                        ))}
-                </>
-            )
-        }
-        else if (page === Math.ceil(count / 3)) {
-            return (
-                <>
-                    <span>. . .</span>
-                    {[...Array(page).keys()]
-                        .filter((item) => ((page + 1) % 3 === 0 ? item >= page - 3 : (page - 1) % 3 === 0 ? item >= page - 2 : item >= page - 1))
-                        .map((item) => (
-                            <button key={item} disabled={item === page} onClick={() => setPage(item)} class="btn rounded-pill btn-outline-brown" type="button">
-                                <span>{item}</span>
-                            </button>
-                        ))}
-                </>
-            )
-        }
-        else if ((page + 1) % 3 === 0) {
-            return (
-                <>
-                    <span>. . .</span>
-                    {[...Array(page + 3).keys()]
-                        .filter((item) => (item >= page))
-                        .filter((item) => (item <= Math.min(page + 2, Math.ceil(count / 3) - 1)))
-                        .map((item) => (
-                            <button key={item} disabled={item === page} onClick={() => setPage(item)} class="btn rounded-pill btn-outline-brown" type="button">
-                                <span>{item}</span>
-                            </button>
-                        ))}
-                </>
-            )
-        } else if ((page - 1) % 3 === 0) {
-            return (
-                <>
-                    <span>. . .</span>
-                    {[...Array(page + 1).keys()]
-                        .filter((item) => (item >= page - 2))
-                        .map((item) => (
-                            <button key={item} disabled={item === page} onClick={() => setPage(item)} class="btn rounded-pill btn-outline-brown" type="button">
-                                <span>{item}</span>
-                            </button>
-                        ))}
-                </>
-            )
-        } else {
-            return (
-                <>
-                    <span>. . .</span>
-                    {[...Array(page + 2).keys()]
-                        .filter((item) => (item >= page - 1))
-                        .filter((item) => (item <= Math.min(page + 1, Math.ceil(count / 3) - 1)))
-                        .map((item) => (
-                            <button key={item} disabled={item === page} onClick={() => setPage(item)} class="btn rounded-pill btn-outline-brown" type="button">
-                                <span>{item}</span>
-                            </button>
-                        ))}
-                </>
-            )
-        }
-    }
 
     return (
         <div id={id} class="carousel slide" data-ride="carousel">
@@ -93,7 +22,7 @@ const RecipeCarousel = ({ recipes, hasEnded, setPage, page, count, id, deleted, 
                 <button disabled={page === 1} onClick={() => setPage(1)} class="btn rounded-pill btn-outline-brown" type="button">
                     <span>1</span>
                 </button>
-                <Pagination />
+                <Pagination count={count} page={page} setPage={setPage} perPage={3}/>
                 {Math.ceil(count / 3) <= 5 ?
                     Math.ceil(count / 3) !== 1 ?
                         <button disabled={page === Math.ceil(count / 3)} onClick={() => setPage(Math.ceil(count / 3))} class="btn rounded-pill btn-outline-brown" type="button">
